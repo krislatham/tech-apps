124deeae088ffb0c9cedb2f59a501d402f54271a
Kris Latham <kris.latham@gmail.com>
2026-01-13 13:22:27 -0600

    Testing

--- main.ts
+++ main.ts
@@ -1,51 +1,126 @@
-//% color=#BF0A30 icon="\uf2b9" block="Texas Icons"
+//% color=#BF5700 icon="\uf2b9" block="Texas Icons"
 namespace texasIcons {
 
-    // Texas Star icon
-    //% fixedInstance
-    export const TexasStar = images.createImage(`
-        . # . # .
-        # . # . #
-        . # . # .
-        # . # . #
-        . # . # .
-    `)
-
-    // Longhorn icon
-    //% fixedInstance
-    export const Longhorn = images.createImage(`
-        # . . . #
-        # # . # #
-        . . # . .
-        . # . # .
-        # . . . #
-    `)
-
-    // Texas Outline icon
-    //% fixedInstance
-    export const TexasOutline = images.createImage(`
-        . # # # .
-        # . . . #
-        # . # # #
+    let longhornFrames = [
+        `
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `,
+        `
+        . # . . .
         # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `,
+        `
+        . . # . .
+        # # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `,
+        `
+        . . . # .
+        # # # . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `,
+        `
+        . . . . #
+        . # # # .
+        # # . . .
+        # . . . .
+        . . . . .
+        `,
+        `
+        . . . . .
+        . . # # #
+        # # # . .
+        # # . . .
+        # . . . .
+        `,
+        `
+        . . . . .
+        . . . # #
+        # # # # .
+        # # # . .
+        . # . . .
+        `,
+        `
+        . . . . .
+        # . . . #
+        # # # # #
+        . # # # .
+        . . # . .
+        `,
+        `
+        . . . . .
+        # # . . .
         . # # # #
-    `)
+        . . # # #
+        . . . # .
+        `,
+        `
+        . . . . .
+        # # # . .
+        . . # # #
+        . . . # #
+        . . . . #
+        `,
+        `
+        # . . . .
+        . # # # .
+        . . . # #
+        . . . . #
+        . . . . .
+        `,
+        `
+        . # . . .
+        . . # # #
+        . . . . #
+        . . . . .
+        . . . . .
+        `,
+        `
+        . . # . .
+        . . . # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `,
+        `
+        . . . # .
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `,
+        `
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `,
+        `
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `
+    ]
 
-    // Show Texas Star
-    //% block="show Texas star"
-    export function showTexasStar() {
-        basic.showImage(TexasStar)
-    }
-
-    // Show Longhorn
-    //% block="show Longhorn"
-    export function showLonghorn() {
-        basic.showImage(Longhorn)
-    }
-
-    // Show Texas Outline
-    //% block="show Texas outline"
-    export function showTexasOutline() {
-        basic.showImage(TexasOutline)
+    //% block="Longhorn"
+    export function scrollLonghorn() {
+        for (let i = 0; i < longhornFrames.length; i++) {
+            images.createImage(longhornFrames[i]).showImage(0)
+            basic.pause(150)
+        }
     }
 }
