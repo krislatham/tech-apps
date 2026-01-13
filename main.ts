243e288700f8d8cb262f96382f617037a317fc94
Kris Latham <kris.latham@gmail.com>
2026-01-13 12:14:11 -0600

    Testing multiple folder structures

---
+++ main.ts
@@ -0,0 +1,30 @@
+//% color=#BF0A30 icon="\uf2b9" block="Texas Icons"
+namespace texasIcons {
+
+    //% fixedInstance
+    export const TexasStar = images.createImage(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+    `)
+
+    //% fixedInstance
+    export const Longhorn = images.createImage(`
+        # . . . #
+        # # . # #
+        . . # . .
+        . # . # .
+        # . . . #
+    `)
+
+    //% fixedInstance
+    export const TexasOutline = images.createImage(`
+        . # # # .
+        # . . . #
+        # . # # #
+        # . . . .
+        . # # # #
+    `)
+}
