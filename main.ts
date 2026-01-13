d0cbd6e33e66ce0e3754db8622a2932b762aaf06
Kris Latham <kris.latham@gmail.com>
2026-01-13 12:34:24 -0600

    Testing

--- main.ts
+++ main.ts
@@ -1,15 +1,17 @@
 //% color=#BF0A30 icon="\uf2b9" block="Texas Icons"
 namespace texasIcons {
 
+    // Texas Star icon
     //% fixedInstance
     export const TexasStar = images.createImage(`
-        . . # . .
-        . # # # .
-        # # # # #
-        . # # # .
-        . . # . .
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
     `)
 
+    // Longhorn icon
     //% fixedInstance
     export const Longhorn = images.createImage(`
         # . . . #
@@ -19,6 +21,7 @@ namespace texasIcons {
         # . . . #
     `)
 
+    // Texas Outline icon
     //% fixedInstance
     export const TexasOutline = images.createImage(`
         . # # # .
@@ -28,8 +31,21 @@ namespace texasIcons {
         . # # # #
     `)
 
+    // Show Texas Star
     //% block="show Texas star"
     export function showTexasStar() {
         basic.showImage(TexasStar)
+    }
+
+    // Show Longhorn
+    //% block="show Longhorn"
+    export function showLonghorn() {
+        basic.showImage(Longhorn)
+    }
+
+    // Show Texas Outline
+    //% block="show Texas outline"
+    export function showTexasOutline() {
+        basic.showImage(TexasOutline)
     }
 }
