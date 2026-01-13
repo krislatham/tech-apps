68f3634069798d7e99881afd931725ba856df28e
Kris Latham <kris.latham@gmail.com>
2026-01-13 15:32:22 -0600

    Test

--- main.ts
+++ main.ts
@@ -3,16 +3,16 @@ namespace texasIcons {
 
     //% block="Cowboy Hat Icon"
     export function scrollCowboyHat() {
-        for (let img of texasIcons.getCowboyHatFrames()) {
-            img.showImage(0)
+        for (let img of texasIcons.cowboyHatFrames) {
+            images.createImage(img).showImage(0)
             basic.pause(150)
         }
     }
 
     //% block="Longhorn Icon"
     export function scrollLonghorn() {
-        for (let img of texasIcons.getLonghornFrames()) {
-            img.showImage(0)
+        for (let img of texasIcons.longhornFrames) {
+            images.createImage(img).showImage(0)
             basic.pause(150)
         }
     }
